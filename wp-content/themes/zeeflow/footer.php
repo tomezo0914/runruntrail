271aa5f6c31d17bbd9d3026cb4e1199795e862aa
Takeshi Toyotome <tomezo0914@gmail.com>
2015-02-01 19:56:25 +0900

    mod footr copyright

--- wp-content/themes/zeeflow/footer.php
+++ wp-content/themes/zeeflow/footer.php
@@ -1,13 +1,7 @@
 		
 		<?php themezee_footer_before(); // hook before #footer ?>
 		<footer id="footer" class="container clearfix" role="contentinfo">
-			<?php 
-				$options = get_option('zeeflow_options');
-				if ( isset($options['themeZee_general_footer']) and $options['themeZee_general_footer'] <> "" ) :
-					echo do_shortcode(wp_kses_post($options['themeZee_general_footer']));
-				endif;
-			?>
-			<div id="credit-link"><?php themezee_credit_link(); ?></div>
+                  <p>&copy;&nbsp;2015,&nbsp;Run Run TRAIL All rights reserved.</p>
 		</footer>
 		<?php themezee_footer_after(); // hook after #footer ?>
 	
